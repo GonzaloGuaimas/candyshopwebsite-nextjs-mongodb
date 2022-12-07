257ea30c224615ef1ff34e6e47a9ec85ec868f08
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-07 15:21:55 -0300

    add primereact style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "primereact/resources/themes/tailwind-light/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";       
+import '../../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
