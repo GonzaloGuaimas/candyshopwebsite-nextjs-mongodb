ce050d1a586d9df375014caa944d9e45de65f2cb
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-07 22:31:19 -0300

    add links for tags a

--- src/components/FooterBar.tsx
+++ src/components/FooterBar.tsx
@@ -6,11 +6,11 @@ export const FooterBar = () => {
     <div className='footer-container'>
       <div>
           <IoLocationSharp  size={'2em'} color={'#000b76'}/>
-          <p>9 de Julio 310</p>
+          <a href="https://goo.gl/maps/BAApxbd1WneaYZv38"><p>9 de Julio 310</p></a>
       </div>
       <div>
           <IoLogoWhatsapp  size={'2em'} color={'#000b76'}/>
-          <p>+54 387 4646 037</p>
+          <a href="https://wa.link/9cptla"><p>+54 387 4646 037</p></a>
       </div>
     </div>
   )
